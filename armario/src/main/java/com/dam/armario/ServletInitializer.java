b42bee50a769cb048fe017988e5c9565233c3b6b
Cjchn <carlosjchamorro@gmail.com>
2024-02-07 09:11:56 +0100

    primer commit

---
+++ armario/src/main/java/com/dam/armario/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dam.armario;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ArmarioApplication.class);
+	}
+
+}
